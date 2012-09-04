54807f79511bbcdd04df1061b83e61053b0fae26
sportsstar89 <sjpirri@gmail.com>
2012-09-04 11:29:24 -0400

    first commit aokp_jb

--- aokp.mk
+++ aokp.mk
@@ -1,13 +1,11 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := ruby
 
-TARGET_BOOTANIMATION_NAME := 540
-
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/ruby/device_ruby.mk)
@@ -21,3 +19,6 @@ PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ruby BUILD_FINGERPRINT=tmous/htc_ruby/ruby:4.0.3/IML74K/373265.3:user/release-keys PRIVATE_BUILD_DESC="2.14.531.3 CL373265 release-keys" BUILD_NUMBER=373265
+
+PRODUCT_PACKAGES += \
+    bootanimation_540_960
