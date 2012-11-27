61e76b5b3bfb88c63b5e9f7027c68a992b9c5bda
Sportsstar89 <sjpirri@gmail.com>
2012-11-27 13:01:39 -0500

    Temp fix for wi-fi and BT

--- device_ruby.mk
+++ device_ruby.mk
@@ -28,7 +28,8 @@ PRODUCT_PACKAGES += \
     lights.ruby
 
 ## The gps config appropriate for this device
-PRODUCT_COPY_FILES += device/common/gps/gps.conf_EU:system/etc/gps.conf
+PRODUCT_COPY_FILES += \
+    device/common/gps/gps.conf_EU:system/etc/gps.conf
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -38,41 +39,30 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
-# Hostapd (Required for Wi-Fi)
-PRODUCT_PACKAGES += \
-    hostapd.conf \
-    dhcpcd.conf \
-    TQS_D_1.7.ini \
-    wpa_supplicant.conf \
-    calibrator \
-    hostapd_cli \
-    hostapd
-
-# Firmware (Required for Wi-Fi)
-PRODUCT_COPY_FILES += \
-    device/htc/ruby/firmware/fmc_init_1273.2.bts:system/etc/firmware/fmc_init_1273.2.bts \
-    device/htc/ruby/firmware/fm_rx_init_1273.2.bts:system/etc/firmware/fm_rx_init_1273.2.bts \
-    device/htc/ruby/firmware/protocols:system/etc/firmware/protocols \
-    device/htc/ruby/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
-    device/htc/ruby/firmware/vac_config.ini:system/etc/firmware/vac_config.ini \
-    device/htc/ruby/firmware/version:system/etc/firmware/version \
-    device/htc/ruby/firmware/WL127x_2.0_2.25.bts:system/etc/firmware/WL127x_2.0_2.25.bts \
-    device/htc/ruby/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    device/htc/ruby/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw 
-
-# ti-connectivity (Required for Wi-Fi)
+# wifi firmware
 PRODUCT_COPY_FILES += \
     device/htc/ruby/firmware/ti-connectivity/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
     device/htc/ruby/firmware/ti-connectivity/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
     device/htc/ruby/firmware/ti-connectivity/wl127x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    device/htc/ruby/firmware/ti-connectivity/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin 
+    device/htc/ruby/firmware/ti-connectivity/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
+    device/htc/ruby/firmware/fmc_init_1273.2.bts:system/etc/firmware/fmc_init_1273.2.bts \
+    device/htc/ruby/firmware/fm_rx_init_1273.2.bts:system/etc/firmware/fm_rx_init_1273.2.bts \
+    device/htc/ruby/firmware/htc_1271fw.bin:system/etc/firmware/htc_1271fw.bin \
+    device/htc/ruby/firmware/htc_1271fw_196_header.bin:system/etc/firmware/htc_1271fw_196_header.bin \
+    device/htc/ruby/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
+    device/htc/ruby/firmware/WL127x_2.0_2.25.bts:system/etc/firmware/WL127x_2.0_2.25.bts \
+    device/htc/ruby/firmware/vac_config.ini:system/etc/firmware/vac_config.ini \
+    device/htc/ruby/firmware/version:system/etc/firmware/version \
+    device/htc/ruby/wifi/firmware.bin:system/etc/wifi/firmware.bin \
+    device/htc/ruby/wifi/firmware_ap.bin:system/etc/wifi/firmware_ap.bin \
+    device/htc/ruby/wifi/hostapd.conf:system/etc/wifi/hostapd.conf \
+    device/htc/ruby/wifi/tiwlan.ini:system/etc/wifi/tiwlan.ini \
+    device/htc/ruby/wifi/tiwlan_ap.ini:system/etc/wifi/tiwlan_ap.ini \
+    device/htc/ruby/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
-# WiFi
+# QC thermald config
 PRODUCT_COPY_FILES += \
-    device/htc/ruby/wifi/95-configured:system/etc/dhcpcd-hooks/95-configured \
-    device/htc/ruby/wifi/p2p_supplicant.conf:system/etc/wifi/p2p_supplicant.conf \
-    device/htc/ruby/wifi/p2p_supplicant_manual.conf:system/etc/wifi/p2p_supplicant_manual.conf \
-    device/htc/ruby/wifi/rt_tables:system/etc/iproute2/rt_tables \
+    device/htc/ruby/configs/thermald.conf:system/etc/thermald.conf
 
 # Ramdisk files
 PRODUCT_COPY_FILES += \
@@ -149,6 +139,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/ruby/firmware/default_bak.acdb:system/etc/firmware/default_bak.acdb \
     device/htc/ruby/firmware/default_rogers_bak.acdb:system/etc/firmware/default_rogers_bak.acdb
+
+# Kernel modules
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+PRODUCT_COPY_FILES += $(shell \
+    find device/htc/ruby/modules -name '*.ko' \
+    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
+    | tr '\n' ' ')
+endif
 
 # misc
 PRODUCT_PROPERTY_OVERRIDES += \
