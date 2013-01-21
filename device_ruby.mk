b0059c74aeea5f3df0c45484a84a3e0d653d7e3d
sportsstar89 <sjpirri@gmail.com>
2013-01-21 11:35:16 -0500

    Revert "Update device_ruby.mk"
    This reverts commit 1bc89113c01c46b1ee450419651927e639c92856.

--- device_ruby.mk
+++ device_ruby.mk
@@ -128,7 +128,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/base/data/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/base/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # misc
 PRODUCT_PROPERTY_OVERRIDES += \
