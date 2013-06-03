472dddaf654ab7c51df55addb0ebbd1bd629f788
Sportsstar89 <sjpirri@gmail.com>
2013-06-03 13:12:56 -0300

    Android.mk: arrange to include only when ruby is selected

--- Android.mk
+++ Android.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(TARGET_DEVICE),ruby)
 LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
+    include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
